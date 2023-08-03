869fd9e3bd69d87a79f097d19e8f5bcc2d52e8fb
PriyadharshiniNilaapps <140385664+PriyadharshiniNilaApps@users.noreply.github.com>
2023-08-03 18:17:44 +0530

    Worked on Assessment - 2

--- AngularJS_Unit2_6/app/app.js
+++ AngularJS_Unit2_6/app/app.js
@@ -9,11 +9,20 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
 // Configure the routes
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
+    .when('/', {
+            templateUrl: 'app/view/login.html',
+            controller: 'loginController'
+        })
+        
+        .when('/register', {
+            templateUrl: 'app/view/register.html',
+            controller: 'registerController'
+        })
         .when('/home', {
             templateUrl: 'app/view/home.html',
             controller: 'homeController'
         })
-        .otherwise({ redirectTo: '/home' });
+        .otherwise({ redirectTo: '/login' });
     console.log($routeProvider);
 
 }]);
---
+++ AngularJS_Unit2_6/app/controller/registerController.js
@@ -0,0 +1,4 @@
+app.controller('registerController', ['$scope', function ($scope) {
+    // Your home controller logic goes here
+}]);
+
