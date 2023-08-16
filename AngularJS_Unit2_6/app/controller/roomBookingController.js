850a08094ba4bd741f66b6d03f9ea2446b0c6a50
PriyadharshiniNilaapps <140385664+PriyadharshiniNilaApps@users.noreply.github.com>
2023-08-16 19:06:38 +0530

    Worked on Assessment - 3

--- AngularJS_Unit2_6/app/controller/roomBookingController.js
+++ AngularJS_Unit2_6/app/controller/roomBookingController.js
@@ -1,10 +1,16 @@
-
-app.controller('roomBookingController',['$scope', function ($scope) {
+app.controller('roomBookingController',['$scope', function ($scope, emailService) {
     $scope.genders = [
         'Male',
         'Female',
         'Transgender',
     ];
+
+    $scope.idType = [
+        'Aadhar card',
+        'Voter ID',
+        'PAN CARD',
+        'Driving Licence'
+    ]
     
     $scope.cateringTypes = [
         'Up to 5 persons VEG',
@@ -19,32 +25,47 @@ app.controller('roomBookingController',['$scope', function ($scope) {
         'Up to 10 clothes - Dry wash',
         'Up to 20 clothes - Dry wash'
 
-    ];
+    ];  
 
-    $scope.validationStatus = '';
-    // $('#charges').modal("show");
-  
-}]).filter('customValidator', function() {
-    return function(input){
+    $scope.email = '';
+    $scope.err = false;
+    $scope.isValidEmail = function(){
+        $scope.err = emailService.isValidEmail1($scope.email);
+    }
+}])
+.filter('customValidator', function() {
+    return function(input,scope){
         
-        if(!input){
-            $scope.validationStatus = 'Name is required';
-          
-            return false;
-        }
-
         if (!(/^[a-zA-Z()]+$/.test(input))) { 
-            $scope.validationStatus = 'Name must be alphabets';
-            return false;
+            scope.validationStatus = 'Name must be alphabets';
+            return true;
         }
 
-        $scope.validationStatus = 'Input is valid.';
-        return true;
+         scope.validationStatus = '';
+        return false;
     };
    
    
-});
-
+})
+.filter('remainingValue', function(){
+    return function(input,scope){
+        if(!input || input === ''){
+            scope.remaining = '';
+            return false;
+        }else{
+        scope.remaining = 50 - input.length + ' characters remaining';
+        return true;
+        }
+        
+    }
+})
+.service('emailService', function(){
+    this.isValidEmail1 = function(email){
+       
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailPattern.test(email);
+    }
+})
 
 
 
