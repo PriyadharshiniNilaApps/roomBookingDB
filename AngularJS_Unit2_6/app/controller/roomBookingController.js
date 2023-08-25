23ce5e52d72a662d3bdfa88fde7b6e2de981177d
PriyadharshiniNilaapps <140385664+PriyadharshiniNilaApps@users.noreply.github.com>
2023-08-25 18:02:09 +0530

    Completed Assessments

--- AngularJS_Unit2_6/app/controller/roomBookingController.js
+++ AngularJS_Unit2_6/app/controller/roomBookingController.js
@@ -37,7 +37,7 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
    var inputModels = [ "fullname", "gender", "email", "idtype", "age", "purpose", "phonenumber", "idnumber", "roomtype", "checkindate", "expectedcheckoutdate", "cateringType", "laundryType", "roomSize", "checkintime", "expectedcheckouttime"]
 
     $scope.values = [];
-    $scope.regex = '/^[a-zA-Z()]+$/'
+    $scope.regex = '/^[a-zA-Z ]+$/'
     $scope.alphabetsOnly = 'Name must be alphabets';
     $scope.genders = ['Male','Female','Transgender'];
     $scope.idType = [ 'Aadhar card', 'Voter ID', 'PAN CARD', 'Driving Licence']
@@ -67,7 +67,6 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
       }
   
     $scope.roomSizes = [ '1 bed room', '2 bed room', '3 bed room', '4 bed room' ];
-    $scope.defaultSelected = "Choose a Room size"
     $scope.cateringTypes = ['Up to 5 persons VEG','Up to 10 persons VEG','Up to 5 persons NON-VEG','Up to 10 persons NON-VEG'];
     $scope.laundryTypes = ['Up to 10 clothes - Normal wash','Up to 20 clothes - Normal wash','Up to 10 clothes - Dry wash','Up to 20 clothes - Dry wash'];  
     $scope.formSubmitted = false;
@@ -102,16 +101,33 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
           minDate.max = maxDateValue;
       }
 
-      $scope.roomSize = 'roomSize';
 
       //Showing the charges form after validating the additional requirements form 
       $scope.submitAllData = function(){
           if($scope.subForm2.$valid){
             $scope.values.push($scope.form);
+            console.log($scope.values);
             $scope.roomBookingDetailsForm = true;
-             for(var i = 0;i< inputModels.length;i++){
-                    $scope.form[inputModels[i]]="";
-             }
+            $scope.form = {
+              fullname:"",
+              gender:"",
+              email:"",
+              idtype:"",
+              age:"",
+              purpose:"",
+              phonenumber:"",
+              idnumber:"",
+              roomtype:"",
+              checkindate:"",
+              expectedcheckoutdate:"",
+              cateringType:"",
+              laundryType:"",
+              roomSize:"",
+              checkintime:"",
+              expectedcheckouttime:"",
+       
+           };
+       
              $scope.subForm1.$setUntouched();
              $scope.subForm2.$setUntouched();
             $('#charges').modal("show");      
@@ -128,7 +144,7 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
 //Filter for field with only alphabets
 .filter('onlyAlphabets', function() {
   return function(input,scope){
-      if (!(/^[a-zA-Z()]+$/.test(input))) { 
+      if (!(/^[a-zA-Z ]+$/.test(input))) { 
         scope.validationStatus = 'Name must be alphabets';
         return true;
       }
@@ -138,7 +154,7 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
 //Filter for field with only alphaNumberics
 .filter('alphaNumerics', function() {
   return function(input,scope){ 
-      if (!(/^[\w]+$/.test(input))) { 
+      if (!(/^[\w ]+$/.test(input))) { 
         scope.validationStatus = 'You can only use alphabets, numbers and underscore in ID number';
         return true;
       }
@@ -153,8 +169,8 @@ app.controller('roomBookingController',['$scope', 'EmailService', '$timeout', fu
     scope: {
         ngModel: '=',
         options: '=',
-        selected: '=',
-        name: '=',
+        selected: '@',
+        name: '@',
     },
   
     template: `
